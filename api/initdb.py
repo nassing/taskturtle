2efd2693c689b63d6bd34eeeff199aff2d26e12e
norman.assing@telecomnancy.eu <norman.assing@telecomnancy.eu>
2023-04-26 19:22:57 +0200

    Base de données

---
+++ api/initdb.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+with sqlite3.connect('database.db') as conn:
+    conn.execute('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, email TEXT UNIQUE, guest_token TEXT UNIQUE, link_to_profile_picture TEXT, balance INTEGER DEFAULT 0)')
+
+    conn.execute('CREATE TABLE transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, sender_id INTEGER NOT NULL, receiver_id INTEGER NOT NULL, amount INTEGER NOT NULL, date TEXT NOT NULL, FOREIGN KEY(sender_id) REFERENCES user(id), FOREIGN KEY(receiver_id) REFERENCES user(id))')
+
+    conn.execute('CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT NOT NULL, reward INTEGER NOT NULL DEFAULT 0, date TEXT NOT NULL, author_id INTEGER NOT NULL, helper_id INTEGER NOT NULL, is_completed INTEGER NOT NULL DEFAULT 0, FOREIGN KEY(author_id) REFERENCES user(id), FOREIGN KEY(helper_id) REFERENCES user(id))')
---
+++ api/utilsdb.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+def getAllUsers():
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users')
+        return cursor.fetchall()
+    
+def getUserById(user_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE id = ?', (user_id,))
+        return cursor.fetchone()
+    
+def getTasksByHelper(user_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tasks WHERE helper_id = ?', (user_id,))
+        return cursor.fetchall()
+    
+def getTasksByAuthor(user_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tasks WHERE author_id = ?', (user_id,))
+        return cursor.fetchall()
+    
+def getTaskById(task_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tasks WHERE id = ?', (task_id,))
+        return cursor.fetchone()
+    
+def getUncompletedTasks():
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tasks WHERE is_completed = 0')
+        return cursor.fetchall()
+    
+def completeTask(task_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT author_id, helper_id, reward FROM tasks WHERE id = ?', (task_id,))
+        author_id, helper_id, reward = cursor.fetchone()
+        if helper_id == None:
+            cursor.execute('UPDATE tasks SET is_completed = 1 WHERE id = ?', (task_id,))
+            cursor.commit()
+        else:
+            cursor.execute('UPDATE tasks SET is_completed = 1 WHERE id = ?', (task_id,))
+            cursor.execute('UPDATE users SET balance = balance - ? WHERE id = ?', (reward, author_id))
+            cursor.execute('UPDATE users SET balance = balance + ? WHERE id = ?', (reward, helper_id))
+            cursor.commit()

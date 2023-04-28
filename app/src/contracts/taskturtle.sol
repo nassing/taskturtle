d36e6ffc4891baa23cc457316110b60a5e3856c6
norman.assing@telecomnancy.eu <norman.assing@telecomnancy.eu>
2023-04-28 21:35:05 +0200

    Version 1 du contrat solidity sans gas fee

---
+++ app/src/contracts/taskturtle.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.8.19;
+
+contract TaskContract {
+
+    struct Task {
+        uint id;
+        address requester;
+        address performer;
+        uint price;
+        bool completed;
+    }
+
+    Task[] public tasks;
+    mapping(uint => bool) public taskExists;
+
+    event TaskCreated(uint taskId, address requester, uint price);
+    event TaskAccepted(uint taskId, address performer);
+    event TaskCompleted(uint taskId, address performer);
+    event PaymentReleased(uint taskId, address performer);
+
+    function createTask(uint _taskId, uint _price) public {
+        require(!taskExists[_taskId], "Task with this id already exists");
+
+        Task memory newTask = Task({
+            id: _taskId,
+            requester: msg.sender,
+            performer: address(0),
+            price: _price,
+            completed: false
+        });
+
+        tasks.push(newTask);
+        taskExists[_taskId] = true;
+
+        emit TaskCreated(_taskId, msg.sender, _price);
+    }
+
+    function acceptTask(uint _taskId) public payable {
+        require(taskExists[_taskId], "Task with this id does not exist");
+        Task storage task = tasks[_taskId];
+
+        require(!task.completed, "Task already completed");
+        require(task.performer == address(0), "Task already accepted by a performer");
+        require(msg.value == task.price, "Incorrect payment amount");
+
+        task.performer = msg.sender;
+
+        emit TaskAccepted(_taskId, msg.sender);
+    }
+
+    function completeTask(uint _taskId) public {
+        require(taskExists[_taskId], "Task with this id does not exist");
+        Task storage task = tasks[_taskId];
+
+        require(task.performer == msg.sender, "Only the performer can complete the task");
+        require(!task.completed, "Task already completed");
+
+        task.completed = true;
+
+        emit TaskCompleted(_taskId, msg.sender);
+    }
+
+    function releasePayment(uint _taskId) public {
+        require(taskExists[_taskId], "Task with this id does not exist");
+        Task storage task = tasks[_taskId];
+
+        require(task.requester == msg.sender, "Only the requester can release the payment");
+        require(task.completed, "Task not completed");
+
+        payable(task.performer).transfer(task.price);
+
+        emit PaymentReleased(_taskId, task.performer);
+    }
+}

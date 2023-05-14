52179b3a101a138acebc1fe2499c6680875cf669
FALL Ismael <ismael.fall@telecomnancy.eu>
2023-05-14 17:11:44 +0200

    Limite caractères title + reward (digits only) + button submit

--- app/src/components/help/AskForHelpPage.js
+++ app/src/components/help/AskForHelpPage.js
@@ -1,71 +1,66 @@
-//Page pour demander de l'aide
+import React, { useState } from 'react';
 
-import React, {useState} from 'react';
-
-export default function AskForHelpPage({username}) {
-
+export default function AskForHelpPage({ username }) {
   const [taskTitle, setTaskTitle] = useState('');
   const [taskDescription, setTaskDescription] = useState('');
   const [taskLocation, setTaskLocation] = useState('');
   const [taskReward, setTaskReward] = useState('');
 
-  function handleSubmit() { 
-    console.log("entering handleSubmit");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('entering handleSubmit');
 
     const data = {
       username: username,
       taskTitle: taskTitle,
       taskDescription: taskDescription,
       taskLocation: taskLocation,
-      taskReward: taskReward
+      taskReward: taskReward,
     };
 
     fetch('http://localhost:4859/submitTask', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-    .then(response => {
-      if (response.ok) {
-        response.text().then(text => {
-          if(text === "") {
-            console.log("Task submitted successfully");
-          }
-          else
-          {
-            console.log(text);
-          }
-        })
-      }
-    })
-    .catch(error => console.log(error.message));
+      .then((response) => {
+        if (response.ok) {
+          response.text().then((text) => {
+            if (text === '') {
+              console.log('Task submitted successfully');
+            } else {
+              console.log(text);
+            }
+          });
+        }
+      })
+      .catch((error) => console.log(error.message));
   }
 
-
-  return(
-  <>
-    <h1>Ask For Help</h1>
-    <form className="help-form">
-      <label>
-        Title:
-        <input type="text" value={taskTitle} onChange={e => setTaskTitle(e.target.value)} />
-      </label>
-      <label>
-        Description:
-        <input type="text" value={taskDescription} onChange={e => setTaskDescription(e.target.value)} />
-      </label>
-      <label>
-        Location:
-        <input type="text" value={taskLocation} onChange={e => setTaskLocation(e.target.value)} />
-      </label>
-      <label>
-        Reward:
-        <input type="text" value={taskReward} onChange={e => setTaskReward(e.target.value)} />
-      </label>
-      <div onClick={() => handleSubmit()}> Submit </div>
-    </form>
-  </>
-  )
+  return (
+    <>
+      <h1>Ask For Help</h1>
+      <form className="help-form" onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input type="text" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} maxLength={35} />
+        </label>
+        <label>
+          Description:
+          <input type="text" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
+        </label>
+        <label>
+          Location:
+          <input type="text" value={taskLocation} onChange={(e) => setTaskLocation(e.target.value)} />
+        </label>
+        <label>
+          Reward:
+          <input type="text" value={taskReward} onChange={(e) => setTaskReward(e.target.value)} pattern="[0-9]*" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
